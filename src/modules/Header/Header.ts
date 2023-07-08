29614a381e83d2004de707b8a83d20dd7ddc3131
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-07-09 00:30:23 +0300

    add Header layout

--- src/modules/Header/Header.ts
+++ src/modules/Header/Header.ts
@@ -1,13 +1,12 @@
 import { Component, ComponentProps } from '../../services/Component';
-import { template } from './template';
+import { getTemplate } from './template';
 import s from './Header.module.scss';
 
 class Header extends Component {
   markup() {
-    return template;
+    return getTemplate(s);
   }
 }
-
 const header = new Header('header', { classes: s.header });
 header.render();
 export default header.element;
--- src/modules/Header/template.ts
+++ src/modules/Header/template.ts
@@ -5,44 +5,44 @@ import minMenu1Img from '../../assets/img/image450_1.jpg';
 import minMenu2Img from '../../assets/img/image450_2.jpg';
 import minMenu3Img from '../../assets/img/image450_3.jpg';
 
-export const template = `
-<div class="container header__container">
-    <a class="header__logo-link" href="#tickets">
-        <div class="logo header__logo"></div>
-        <h1 class="header__title">Louvre</h1>
+export const getTemplate = (styles:any) => `
+<div class="container ${styles.header__container}">
+    <a class="${styles['header__logo-link']}" href="#tickets">
+        <div class="logo ${styles.header__logo}"></div>
+        <h1 class="${styles.header__title}">Louvre</h1>
     </a>
-    <div class="header__burger">
+    <div class="${styles.header__burger}">
         <span></span>
     </div>
-    <div class="header__main-menu">
-        <nav class="header__nav">
-            <ul class="header__list">
-                <li class="header__item"><a class="header__link" href="#visiting">Visiting</a></li>
-                <li class="header__item"><a class="header__link" href="#explore">Explore</a></li>
-                <li class="header__item"><a class="header__link" href="#video">Video</a></li>
-                <li class="header__item"><a class="header__link" href="#gallery">Gallery</a></li>
-                <li class="header__item"><a class="header__link" href="#tickets">Tickets</a></li>
-                <li class="header__item"><a class="header__link" href="#contacts">Contacts</a></li>
+    <div class="${styles['header__main-menu']}">
+        <nav class="${styles.header__nav}">
+            <ul class="${styles.header__list}">
+                <li class="${styles.header__item}"><a class="${styles.header__link}" href="#visiting">Visiting</a></li>
+                <li class="${styles.header__item}"><a class="${styles.header__link}" href="#explore">Explore</a></li>
+                <li class="${styles.header__item}"><a class="${styles.header__link}" href="#video">Video</a></li>
+                <li class="${styles.header__item}"><a class="${styles.header__link}" href="#gallery">Gallery</a></li>
+                <li class="${styles.header__item}"><a class="${styles.header__link}" href="#tickets">Tickets</a></li>
+                <li class="${styles.header__item}"><a class="${styles.header__link}" href="#contacts">Contacts</a></li>
             </ul>
         </nav>
-        <div class="header__menu-images">
-            <picture class="header__image">
+        <div class="${styles['header__menu-images']}">
+            <picture class="${styles}.header__image">
                 <source media="(max-width: 450px)" srcset="${minMenu1Img}"/>
                 <img src="${menu1Img}" alt="" />
             </picture>
-            <picture class="header__image">
+            <picture class="${styles['header__menu-images']}">
                 <source media="(max-width: 450px)" srcset="${minMenu2Img}"/>
                 <img src="${menu2Img}" alt="" />
             </picture>
-            <picture class="header__image">
+            <picture class="${styles['header__menu-images']}">
                 <source media="(max-width: 450px)" srcset="${minMenu3Img}"/>
                 <img src="${menu3Img}" alt="" />
             </picture>
 
 
         </div>
-        <div class="header__line"></div>
-        <ul class="social-list header__social-list">
+        <div class="${styles.header__line}"></div>
+        <ul class="social-list ${styles['header__social-list']}">
             <li class="social-list__social-item">
                 <a class="social-list__soc-link social-list__soc-link_youtube"
                     href="https://www.youtube.com/user/louvre" target="_blank"></a>
