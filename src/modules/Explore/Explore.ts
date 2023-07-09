0d0289ab3ba164e5bdfadd52b5cfe8a9ff9ebdeb
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-07-09 22:16:17 +0300

    add slider into explorer

--- src/modules/Explore/Explore.ts
+++ src/modules/Explore/Explore.ts
@@ -1,13 +1,15 @@
+/* eslint-disable no-inner-declarations */
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import { Component, ComponentProps } from "../../services/Component";
 import { getTemplate } from "./template";
 import s from "./Explore.module.scss";
 
 class Explore extends Component {
-  welcomeSlider: any;
+  slider: HTMLElement | null;
 
   constructor(target = "section", props: ComponentProps) {
     super(target, props);
-    this.welcomeSlider = null;
+    this.slider = null;
   }
 
   markup() {
@@ -16,6 +18,64 @@ class Explore extends Component {
   }
 
   initSlider() {
+    const overlay: HTMLElement | null = document.querySelector(`.${s["explore-slider__new-image"]}`);
+    this.slider = document.querySelector(`.${s["explore-slider__separator"]}`);
+
+    const compareImages = (img: HTMLElement) => {
+      if (this.slider) {
+        let clicked = 0;
+        const w = img.offsetWidth;
+        const wid = +window.getComputedStyle(this.slider as Element, null).getPropertyValue('width').slice(0, -2);
+        img.style.width = `${(w / 2) + 20}px`;
+        this.slider.addEventListener("pointerdown", slideReady);
+        window.addEventListener("pointerup", slideFinish);
+
+        function slideReady(evt: MouseEvent) {
+          evt.preventDefault();
+          clicked = 1;
+          window.addEventListener("pointermove", slideMove);
+        }
+
+        function slideFinish() {
+          clicked = 0;
+        }
+
+        function slideMove(evt: MouseEvent) {
+          let pos;
+          if (!clicked) {
+            return null;
+          }
+
+          pos = getCursorPos(evt);
+
+          if (pos < 0) {
+            pos = 0;
+          }
+          if (pos > w) {
+            pos = w;
+          }
+          slide(pos);
+        }
+
+        function getCursorPos(evt: MouseEvent) {
+          let x = 0;
+          evt = evt || window.event;
+          const a = img.getBoundingClientRect();
+          x = evt.pageX - a.left;
+          x -= window.pageXOffset;
+          return x;
+        }
+
+        const slide = (x: number) => {
+          img.style.width = `${x}px`;
+          if (this.slider) this.slider.style.left = `${img.offsetWidth - this.slider.offsetWidth / 2 + wid / 2 - 20}px`;
+        };
+      }
+    };
+
+    if (overlay) {
+      compareImages(overlay);
+    }
   }
 
   componentDidMount() {
@@ -23,8 +83,6 @@ class Explore extends Component {
   }
 }
 
-const explore = new Explore("section", {
+export const explore = new Explore("section", {
   classes: s.explore,
 });
-
-export { explore };
