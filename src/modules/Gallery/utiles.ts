b8a79ef966345cf70696663fd36be6ba8eece518
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-07-21 12:10:29 +0300

    fix gallery source

--- src/modules/Gallery/utiles.ts
+++ src/modules/Gallery/utiles.ts
@@ -13,30 +13,48 @@ import image12 from "../../assets/img/galery15_sm.jpg";
 import image13 from "../../assets/img/galery12_ss.jpg";
 import image14 from "../../assets/img/galery11_sm.jpg";
 import image15 from "../../assets/img/galery2_sl.jpg";
+
+import m_image1 from "../../assets/img/m_galery9_sl.jpg";
+import m_image2 from "../../assets/img/m_galery4_sm.jpg";
+import m_image3 from "../../assets/img/m_galery6_sl.jpg";
+import m_image4 from "../../assets/img/m_galery13__ss.jpg";
+import m_image5 from "../../assets/img/m_galery1_sm.jpg";
+import m_image6 from "../../assets/img/m_galery8_sl.jpg";
+import m_image7 from "../../assets/img/m_galery3_sl.jpg";
+import m_image8 from "../../assets/img/m_galery5_sl.jpg";
+import m_image9 from "../../assets/img/m_galery14_sl.jpg";
+import m_image10 from "../../assets/img/m_galery7_sl.jpg";
+import m_image11 from "../../assets/img/m_galery10_sm.jpg";
+import m_image12 from "../../assets/img/m_galery15_sm.jpg";
+import m_image13 from "../../assets/img/m_galery12_ss.jpg";
+import m_image14 from "../../assets/img/m_galery11_sm.jpg";
+import m_image15 from "../../assets/img/m_galery2_sl.jpg";
 import styles from "./Gallery.module.scss";
 
+type linksObjType = { link: string, shortlink: string };
+
 const imagesLinks = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
-  image13,
-  image14,
-  image15,
+  { link: image1, shortlink: m_image1 },
+  { link: image2, shortlink: m_image2 },
+  { link: image3, shortlink: m_image3 },
+  { link: image4, shortlink: m_image4 },
+  { link: image5, shortlink: m_image5 },
+  { link: image6, shortlink: m_image6 },
+  { link: image7, shortlink: m_image7 },
+  { link: image8, shortlink: m_image8 },
+  { link: image9, shortlink: m_image9 },
+  { link: image10, shortlink: m_image10 },
+  { link: image11, shortlink: m_image11 },
+  { link: image12, shortlink: m_image12 },
+  { link: image13, shortlink: m_image13 },
+  { link: image14, shortlink: m_image14 },
+  { link: image15, shortlink: m_image15 },
 ];
 
 const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-const shuffle = (arr: string[]) => {
+const shuffle = (arr:linksObjType[]) => {
   const result = [];
 
   while (arr.length > 0) {
@@ -49,17 +67,12 @@ const shuffle = (arr: string[]) => {
 export const shuffledImagesLinks = shuffle(imagesLinks);
 
 export const getChildren = async () => {
-  const postImage = async (link: string) => {
+  const postImage = async (linksObj: linksObjType) => {
+    const { link, shortlink } = linksObj;
     const img = document.createElement("img");
     img.classList.add(`${styles.gallery__image}`);
-    const imgUrl = link;
-    img.dataset.src = imgUrl;
-   
-    const regex = /^(.*\/)([^/]+)$/;
-    const newFilename = `${new URL(link, import.meta.url).href.replace(regex, '$1m_$2')}`;
-
-    img.src = (await import(newFilename)).default;
-
+    img.dataset.src = link;
+    img.src = shortlink;
     img.alt = "";
     return img;
   };
